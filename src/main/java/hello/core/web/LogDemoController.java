290006e768bca966b96037d90a4b7204aa9fdff7
mingyo <alsry922@gmail.com>
2022-10-28 02:10:20 +0900

    proxyMode 설정해서 가짜 프록시 모드를 미리 주입함으로써 해결 가능

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,13 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
   private final LogDemoService logDemoService;
-  private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+  private final MyLogger myLogger;
 
   @RequestMapping("log-demo")
   @ResponseBody
   public String logDemo(HttpServletRequest request) {
     String requestURL = request.getRequestURL().toString();
-    MyLogger myLogger = myLoggerObjectProvider.getObject();
     myLogger.setRequestUrl(requestURL);
 
     myLogger.log("controller test");
